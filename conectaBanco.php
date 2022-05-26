cda206a714575d15e56d5358778e1543d81a762a
Thiago Nascimento <thiagosilvanascimento2015@gmail.com>
2022-05-26 10:38:02 -0300

    Alterações no arquivo conectaBanco

--- conectaBanco.php
+++ conectaBanco.php
@@ -6,11 +6,11 @@ define('DBNAME', 'agenda');
 define('PORT', '3306');
 
 $conexao = new pdo('mysql: host=' . HOST . '; port=' . PORT . '; dbname=' . DBNAME, USER, PASS );
-
+/*
 try {
     $conexao = new pdo('mysql: host=' . HOST . '; port=' . PORT . '; dbname=' . DBNAME, USER, PASS );
     echo "Conexão realizada com sucesso";
 } catch(PDOException $exception) {
     echo "Erro ao conectar com o banco: " . $exception->getMessage();
-}
+}*/
 ?>
