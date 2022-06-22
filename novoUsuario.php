13fe7c62ed63bf4657db30a7bbfdd734973ec413
Thiago Nascimento <thiagosilvanascimento2015@gmail.com>
2022-06-22 18:04:39 -0300

    Alterações no arquivo novoUsuario

--- novoUsuario.php
+++ novoUsuario.php
@@ -39,6 +39,8 @@ include "conectaBanco.php";
                 <div class="col-sm"></div>
                 <div class="col-sm-10">
                     <?php
+                    $flagErro = False;
+
                     if (isset($_POST['acao'])) {
                         $acao = $_POST['acao'];
 
@@ -49,16 +51,70 @@ include "conectaBanco.php";
                             $senhaUsuario = $_POST['senhaUsuario'];
                             $senha2Usuario = $_POST['senha2Usuario'];
 
-                            $senhaUsuarioMD5 = md5($senhaUsuario);
+                            if (
+                                !empty($nomeUsuario) && !empty($mailUsuario) && !empty($mail2Usuario) && !empty($senhaUsuario)
+                                && !empty($senha2Usuario)
+                            ) {
 
-                            $sqlNovoUsuario = "INSERT INTO usuarios (nomeUsuario, mailUsuario, senhaUsuario) VALUES(:nomeUsuario, :mailUsuario, :senhaUsuario)";
+                                if ($mailUsuario == $mail2Usuario && $senhaUsuario == $senha2Usuario) {
 
-                            $sqlNovoUsuarioST = $conexao->prepare($sqlNovoUsuario);
-                            $sqlNovoUsuarioST->bindValue(':nomeUsuario', $nomeUsuario);
-                            $sqlNovoUsuarioST->bindValue(':mailUsuario', $mailUsuario);
-                            $sqlNovoUsuarioST->bindValue(':senhaUsuario', $senhaUsuarioMD5);
-                            
-                            $sqlNovoUsuarioST->execute();
+                                    if (strlen($nomeUsuario) >= 5 && strlen($senhaUsuario >= 8)) {
+
+                                        $sqlUsuarios = "SELECT codigoUsuario FROM usuarios WHERE mailUsuario=:mailUsuario";
+
+                                        $sqlUsuariosST = $conexao->prepare($sqlUsuarios);
+
+                                        $sqlUsuariosST->bindValue(':mailUsuario', $mailUsuario);
+
+                                        $sqlUsuariosST->execute();
+                                        $quantidadeUsuarios = $sqlUsuariosST->rowCount();
+
+                                        if ($quantidadeUsuarios == 0) {
+                                            $senhaUsuarioMD5 = md5($senhaUsuario);
+
+                                            $sqlNovoUsuario = "INSERT INTO usuarios (nomeUsuario, mailUsuario, senhaUsuario) VALUES(:nomeUsuario, :mailUsuario, :senhaUsuario)";
+
+                                            $sqlNovoUsuarioST = $conexao->prepare($sqlNovoUsuario);
+                                            $sqlNovoUsuarioST->bindValue(':nomeUsuario', $nomeUsuario);
+                                            $sqlNovoUsuarioST->bindValue(':mailUsuario', $mailUsuario);
+                                            $sqlNovoUsuarioST->bindValue(':senhaUsuario', $senhaUsuarioMD5);
+
+                                            if ($sqlNovoUsuarioST->execute()) {
+
+                                                $mensagemAcao = "Novo usuário cadastrado com sucesso";
+                                            } else {
+                                                $flagErro = True;
+                                                $mensagemAcao = "Código erro: " . $sqlNovoUsuarioST->errorCode();
+                                            }
+                                        } else {
+                                            $flagErro = True;
+                                            $mensagemAcao = "E-mail já cadastrado para outro usuário.";
+                                        }
+                                    } else {
+                                        $flagErro = True;
+                                        $mensagemAcao = "Informe a quantidade mínima de caracteres para cada campo: Nome (5), Senha (8).";
+                                    }
+                                } else {
+                                    $flagErro = True;
+                                    $mensagemAcao = "Os campos de confirmação de e-mail e senha devem ser preenchidos com os respectivos valores.";
+                                }
+                            } else {
+                                $flagErro = True;
+                                $mensagemAcao = "Preencha todos os campos obrigatórios (*).";
+                            }
+
+                            if (!$flagErro) {
+                                $classeMensagem = "alert-success";
+                            } else {
+                                $classeMensagem = "alert-danger";
+                            }
+
+                            echo "<div class=\"alert $classeMensagem alert-dismissible fade show\" role=\"alert\">
+                                    $mensagemAcao
+                                    <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Fechar\">
+                                        <span aria-hidden=\"true\">&times;</span>
+                                    </button>
+                                </div>";
                         }
                     }
                     ?>
@@ -77,7 +133,7 @@ include "conectaBanco.php";
                                                 <i class="bi-people-fill"></i>
                                             </div>
                                         </div>
-                                        <input type="text" class="form-control" id="nomeUsuario" name="nomeUsuario" placeholder="Digite seu nome" required>
+                                        <input type="text" class="form-control" id="nomeUsuario" name="nomeUsuario" placeholder="Digite seu nome" value="<?= ($flagErro) ? $nomeUsuario : "" ?>" required>
                                     </div>
                                 </div>
                                 <div class="row">
@@ -90,7 +146,7 @@ include "conectaBanco.php";
                                                         <i class="bi-at"></i>
                                                     </div>
                                                 </div>
-                                                <input type="text" class="form-control" id="mailUsuario" name="mailUsuario" placeholder="Digite seu e-mail" required>
+                                                <input type="email" class="form-control" id="mailUsuario" name="mailUsuario" placeholder="Digite o seu e-mail" value="<?= ($flagErro) ? $mailUsuario : "" ?>" required>
                                             </div>
                                         </div>
                                     </div>
@@ -103,7 +159,8 @@ include "conectaBanco.php";
                                                         <i class="bi-at"></i>
                                                     </div>
                                                 </div>
-                                                <input type="text" class="form-control" id="mail2Usuario" name="mail2Usuario" placeholder="Repita seu e-mail" required>
+                                                <input type="email" class="form-control" id="mail2Usuario" name="mail2Usuario" placeholder="Repita o seu e-mail" value="<?= ($flagErro) ? $mail2Usuario : ""
+                                                                                                                                                                        ?>" required>
                                             </div>
                                         </div>
                                     </div>
@@ -118,7 +175,7 @@ include "conectaBanco.php";
                                                         <i class="bi-key-fill"></i>
                                                     </div>
                                                 </div>
-                                                <input type="password" class="form-control" id="senhaUsuario" name="senhaUsuario" placeholder="Digite sua senha" required>
+                                                <input type="password" class="form-control" id="senhaUsuario" name="senhaUsuario" placeholder="Digite sua senha" value="<?= ($flagErro) ? $senhaUsuario : "" ?>" required>
                                             </div>
                                         </div>
                                     </div>
@@ -131,7 +188,7 @@ include "conectaBanco.php";
                                                         <i class="bi-key-fill"></i>
                                                     </div>
                                                 </div>
-                                                <input type="password" class="form-control" id="senha2Usuario" name="senha2Usuario" placeholder="Repita sua senha" required>
+                                                <input type="password" class="form-control" id="senha2Usuario" name="senha2Usuario" placeholder="Repita sua senha" value="<?= ($flagErro) ? $senha2Usuario : "" ?>" required>
                                             </div>
                                         </div>
                                     </div>
