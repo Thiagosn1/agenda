3cab81a34e0e235bbb85fa187d3439bdc567a627
Thiago Nascimento <thiagosilvanascimento2015@gmail.com>
2022-06-22 18:53:43 -0300

    Alterações no arquivo listaContatos

--- listaContatos.php
+++ listaContatos.php
@@ -1,3 +1,18 @@
+<?php
+    session_start();
+
+    $verificaUsuarioLogado = $_SESSION['verificaUsuarioLogado'];
+
+    if (!$verificaUsuarioLogado){
+        header("Location: index.php?codMsg=003");
+    } else{
+        include 'conectaBanco.php';
+
+        $nomeUsuarioLogado = $_SESSION['nomeUsuarioLogado'];
+        $codigoUsuarioLogado = $_SESSION['codigoUsuarioLogado'];
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -70,6 +85,9 @@
                     <input class="form-control mr-sm-2" type="search" name="busca" placeholder="Pesquisar">
                     <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Pesquisar</button>
                 </form>
+                <span class="navbar-text ml-4">
+                        Olá <b><?= $nomeUsuarioLogado ?></b>, seja bem-vindo(a)!
+                    </span>
             </div>
         </div>
     </nav>
@@ -95,34 +113,57 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <th scope="row">{id}</th>
-                                        <td>{nome}</td>
-                                        <td>{telefone}</td>
-                                        <td>{email}</td>
-                                        <td>
-                                            <div class="dropdown">
-                                                <a class="btn btn-secondary dropdown-toggle btn-sm" href="#"
-                                                    role="button" id="{id}" data-toggle="dropdown" aria-haspopup="true"
-                                                    aria-expanded="false">
-                                                    Ações
-                                                </a>
+                                    <?php
+                                        if (isset($_GET['busca'])){
+                                            $busca = '%' .  $_GET['busca'] . '%';
+                                        } else {
+                                            $busca = '%%';
+                                        }
 
-                                                <div class="dropdown-menu" aria-labelledby="{id}">
-                                                    <a class="dropdown-item" href="#" data-toggle="modal"
-                                                        data-target="#visualizarContato" data-whatever="{id">
-                                                        <i class="bi-eye"></i> Visualizar
+                                        $sqlContatos = "SELECT codigoContato, nomeContato, mailContato, telefone1Contato FROM contatos WHERE    codigoUsuario=:codigoUsuario AND nomeContato LIKE %:busca% ORDER BY nomeContato";
+
+                                        $sqlContatosST = $conexao->prepare($sqlContatos);
+                                        $sqlContatosST->bindValue(':codigoUsuario', $codigoUsuarioLogado);
+                                        $sqlContatosST->bindValue(':busca', $busca);
+
+                                        $sqlContatosST->execute();
+                                        $quantidadeContatos = $sqlContatosST->rowCount();
+
+                                        if ($quantidadeContatos > 0) {
+                                            $resultadoContatos = $sqlContatosST->fetchALL();
+
+                                            foreach($resultadoContatos as list($codigoContato, $nomeContato, $mailContato, $telefone1Contato)){
+                                            echo "<tr>
+                                            <th scope=\"row\">$codigoContato</th>
+                                            <td>$nomeContato</td>
+                                            <td>$telefone1Contato</td>
+                                            <td>$mailContato</td>
+                                            <td>
+                                                <div class=\"dropdown\">
+                                                    <a class=\"btn btn-secondary dropdown-toggle btn-sm\" href=\"#\"
+                                                        role=\"button\" id=\"{id}\" data-toggle=\"dropdown\" aria-haspopup=\"true\"
+                                                        aria-expanded=\"false\">
+                                                        Ações
                                                     </a>
-                                                    <a class="dropdown-item" href="#">
-                                                        <i class="bi-pencil"></i> Editar
-                                                    </a>
-                                                    <a class="dropdown-item" href="#">
-                                                        <i class="bi-trash"></i> Excluir
-                                                    </a>
+    
+                                                    <div class=\"dropdown-menu\" aria-labelledby=\"{id}\">
+                                                        <a class=\"dropdown-item\" href=\"#\" data-toggle=\"modal\"
+                                                            data-target=\"#visualizarContato\" data-whatever=\"$codigoContato\">
+                                                            <i class=\"bi-eye\"></i> Visualizar
+                                                        </a>
+                                                        <a class=\"dropdown-item\" href=\"cadastroContato.php\">
+                                                            <i class=\"bi-pencil\"></i> Editar
+                                                        </a>
+                                                        <a class=\"dropdown-item\" href=\"#\">
+                                                            <i class=\"bi-trash\"></i> Excluir
+                                                        </a>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                            </td>
+                                        </tr>";
+                                            }
+                                        }
+                                    ?>
                                 </tbody>
                             </table>
                         </div>
@@ -157,20 +198,21 @@
     </div>
     <div class="modal fade" id="visualizarContato" tabindex="-1" role="dialog" aria-labelledby="visualizarDadosContatos"
         aria-hidden="true">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="visualizarDadosContatos">Dados Do Contato</h5>
+                    <h4 class="modal-title" id="visualizarDadosContatos">Dados do contato</h4>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Fechar">
                         <span aria-hidden="true">
                             &times;
                         </span>
                     </button>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body" id="dadosContato">
+
                 </div>
                 <div class="modal-footer">
-                    <button class=" btn btn-secondary" type="button" data-dismiss="modal">
+                    <button class="btn btn-outline-secondary" type="button" data-dismiss="modal">
                         Fechar
                     </button>
                 </div>
@@ -178,5 +220,15 @@
         </div>
     </div>
 </body>
+<script>
+    $(document).ready(function() {
+        $('#visualizarContato').on('show.bs.modal', function (event) {
+            var origemContato = $(event.relatedTarget);
+            var codigoContato = origemContato.data('whatever');
+
+            $('#dadosContato').load('visualizaContato.php?codigoContato=' + codigoContato);
+        });
+    });
+</script>
 
 </html>
